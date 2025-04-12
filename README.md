181b39b0c45732c3e13d3a0676c3b26f46fb4af5
Kendeki <92603580+Kendeki@users.noreply.github.com>
2025-04-12 19:12:30 -0300

    mensagem

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Github-Teste
 teste do uso colaborativo do github
 me contrata pfv
+nao contrata ele
---
+++ rodrigo.py
@@ -0,0 +1,6 @@
+if "rodrigo" == "rodrigo":
+    print("rodrigo")
+
+def rodrigo():
+    rodrigo = "rodrigo"
+    return rodrigo
